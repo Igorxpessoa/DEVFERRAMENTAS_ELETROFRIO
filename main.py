ee646f5e9bf330699c3448e8593fb16837f9a577
Igorxpessoa <108377912+Igorxpessoa@users.noreply.github.com>
2025-05-02 17:21:58 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
-from fastapi import FastAPI
-import subprocess
+# main.py
+from fastapi import FastAPI, Request
+from automacao_retorno_obra_api import BootRetornoObra
+import os
 
 app = FastAPI()
 
 @app.post("/run_ln")
-def run_script():
+async def run_script(request: Request):
     try:
-        # Executa o script como subprocesso
-        subprocess.run(["python", "automacao_retorno_obra_api.py"], check=True)
-        return {"status": "Script executado com sucesso!"}
-    except subprocess.CalledProcessError as e:
-        return {"status": "Erro ao executar o script", "detalhes": str(e)}
+        # Recebendo JSON com login/senha
+        data = await request.json()
+        username = data.get("username")
+        password = data.get("password")
+
+        if not username or not password:
+            return {"status": "erro", "mensagem": "Credenciais ausentes"}
+
+        bot = BootRetornoObra(username, password)
+        resultado = bot.automate_ln()
+
+        return {"status": "sucesso", "message": resultado}
+
+    except Exception as e:
+        return {"status": "erro", "mensagem": str(e)}
