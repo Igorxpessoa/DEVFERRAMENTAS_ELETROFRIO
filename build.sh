9430cf84e72e8d99ba2cbfaf242ec6044260dd11
Igorxpessoa <108377912+Igorxpessoa@users.noreply.github.com>
2025-05-06 10:38:42 -0300

    Add files via upload

--- build.sh
+++ build.sh
@@ -25,8 +25,7 @@ apt-get update && apt-get install -y \
 # Instala o Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 apt-get update
-apt-get install -y ./google-chrome-stable_current_amd64.deb
-rm google-chrome-stable_current_amd64.deb
+apt-get install -y chromium
 
 # Instala dependências
 apt-get install -y unzip curl
